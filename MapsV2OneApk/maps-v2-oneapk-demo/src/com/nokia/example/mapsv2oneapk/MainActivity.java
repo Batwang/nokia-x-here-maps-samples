cdf0c218dc2e315c1310a497ff89db79ba14b3a8
riku salkia <ext-riku.salkia@nokia.com>
2014-03-31 14:18:49 +0300

    AdMob demo ad

--- MapsV2OneApk/maps-v2-oneapk-demo/src/com/nokia/example/mapsv2oneapk/MainActivity.java
+++ MapsV2OneApk/maps-v2-oneapk-demo/src/com/nokia/example/mapsv2oneapk/MainActivity.java
@@ -14,7 +14,11 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends FragmentActivity {
     private static final String HERE_LIBRARY = "com.here.android";
@@ -24,12 +28,21 @@ public class MainActivity extends FragmentActivity {
     private static final String[] REQUIRED_KEYS_ARRAY = {"com.here.android.maps.appid", "com.here.android.maps.apptoken", "com.google.android.maps.v2.API_KEY"};
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    /**
+     * The view to show the ad.
+     */
+    private AdView adView;
+
+    /* Your ad unit id. Replace with your actual ad unit id. */
+    private static final String AD_UNIT_ID = "YOUR_AD_UNIT_IT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         if (validateMetaData()) {
+            addAd();
             addMapFragment();
         } else {
             Toast.makeText(this, R.string.missing_metadata, Toast.LENGTH_LONG).show();
@@ -37,6 +50,58 @@ public class MainActivity extends FragmentActivity {
             return;
         }
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (adView != null) {
+            adView.resume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (adView != null) {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    /**
+     * Called before the activity is destroyed.
+     */
+    @Override
+    public void onDestroy() {
+        // Destroy the AdView.
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    private void addAd() {
+        // Create an ad.
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId(AD_UNIT_ID);
+
+        // Add the AdView to the view hierarchy. The view will have no size
+        // until the ad is loaded.
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+        layout.addView(adView);
+
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device.
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("7249C9EBCC2A21FE34695E730C60DBFA")
+                .addTestDevice("18915ACB769919CF2C428F8ED54C0FFD")
+                .addTestDevice("AC5756D1952302521330EF49D1C0EC5D")
+                .build();
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest);
     }
 
     private void addMapFragment() {
